5bf38beda72cbf0fef0d215cfade448d5d06dc4b
Jeferson Carvalho <jefersonwilliansk8@hotmail.com>
2022-04-19 23:03:46 -0300

    first ionic course by cadmus

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'curso-ionic-cadmus',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
