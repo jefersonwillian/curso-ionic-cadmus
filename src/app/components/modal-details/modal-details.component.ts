dbc8b2b7ec973ecc7975b3fa16b593085a2023fa
Jeferson Carvalho <ext.jeferson.jc@gmail.com>
2022-04-28 23:53:29 -0300

    criação modal de detalhes e estilização da home

---
+++ src/app/components/modal-details/modal-details.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-details',
+  templateUrl: './modal-details.component.html',
+  styleUrls: ['./modal-details.component.scss'],
+})
+export class ModalDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
