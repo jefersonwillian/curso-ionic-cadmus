40b6699ede4c6f88dbf3329f9d7f9db083855d9b
Jeferson Carvalho <ext.jeferson.jc@gmail.com>
2022-04-30 11:39:29 -0300

    Infinite Scroll

---
+++ src/app/@core/utils/infinite-scroll.utils.ts
@@ -0,0 +1,26 @@
+import { IInfiniteScrollTarget } from '../interfaces/infinite-scroll-target.interface/infinite-scroll-target.interface';
+
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+export function infiniteScrollLocal(
+  activeList: any[],
+  fullList: any[],
+  limitList = 20,
+  infiniteTarget?: any
+) {
+  let index = activeList.length;
+  const limit = activeList.length + limitList;
+
+
+  for (index; index < limit && index < fullList.length; index++) {
+    activeList.push(fullList[index]);
+  }
+
+  if (infiniteTarget) {
+    infiniteTarget.complete();
+    if (activeList.length === fullList.length) {
+      infiniteTarget.disabled = true;
+    }
+  }
+
+  return activeList;
+}
